e21419d8d6b7184c6262b02933a1502abbf968fd
FedorT <ftekanov@bk.ru>
2024-11-30 13:21:48 +0300

    Update Task3.2.c

--- Task3.2.c
+++ Task3.2.c
@@ -4,57 +4,59 @@
 
 /**
  * @brief Функция для ввода значения типа int с сообщением.
- * Эта функция запрашивает у пользователя ввод целого числа, выводя заданное сообщение.
- * Если ввод некорректен, программа выводит сообщение об ошибке и завершает выполнение.
  * @param msg Сообщение, которое будет выведено перед вводом значения.
  * @return Введенное значение типа int.
- */
-long inputInt(const char* msg);
-
-/**
- * @brief Функция для ввода значения типа double с сообщением.
- * Эта функция запрашивает у пользователя ввод числа с плавающей точкой, выводя заданное сообщение.
  * Если ввод некорректен, программа выводит сообщение об ошибке и завершает выполнение.
- * @param msg Сообщение, которое будет выведено перед вводом значения.
- * @return Введенное значение типа double.
  */
+int inputInt(const char* msg);
+
 double inputDouble(const char* msg);
 
-int main() {
+/**
+ * @brief Вычисляет сумму членов последовательности.
+ * @param n Число элементов последовательности.
+ */
+double getSumOfSequence(int n);
+
+/**
+ * @brief Вычисляет сумму членов последовательности,
+ *        которые выше по модулю чем epsilon.
+ * @param n Число элементов последовательности.
+ * @param eps Значение epsilon.
+ */
+double getSumOfSequenceMoreThanEps(int n, double eps);
+
+/**
+ * @brief Точка входа в программу.
+ * @return 0 в случае успеха.
+ */
+int main(void) {
     int n = inputInt("n: ");
     double eps = inputDouble("e: ");
-    double res = 0.0;
-    double term = -1.0;
-    double resLessThanEps = 0.0;
-    
-    // Цикл для вычисления суммы ряда
-    for (int k = 1; k <= n; k++) {
-        res += term; // Добавляем текущий член к результату
-        if (abs(term) >= eps) {
-            resLessThanEps += term; // Если член больше или равен eps, добавляем его к другой сумме
-        }
-        term *= -1.0 / k; // Вычисляем следующий член ряда
+    if (eps <= 0) {
+        errno = EIO;
+        perror("epsilon should be greater than 0");
+        return EXIT_FAILURE;
     }
-    
-    printf("sum from k=0 to k=n is equal to %8.5lf\n", res);
-    printf("sum from k=0 to k=n where term greater or equal than %8.5lf is equal to %8.5lf\n", eps, resLessThanEps);
-    
+    double sumOfSeq = getSumOfSequence(n);
+    double sumOfSeqMoreThanEps = getSumOfSequenceMoreThanEps(n, eps);
+    printf("sum from k=0 to k=n is equal to %8.5lf\n", sumOfSeq);
+    printf("sum from k=0 to k=n where term greater or equal than %8.5lf is equal to %8.5lf\n",
+        eps, sumOfSeqMoreThanEps);
     return 0;
 }
 
 /**
  * @brief Функция для ввода значения типа int с сообщением.
- * Эта функция запрашивает у пользователя ввод целого числа, выводя заданное сообщение.
- * Если ввод некорректен, программа выводит сообщение об ошибке и завершает выполнение.
  * @param msg Сообщение, которое будет выведено перед вводом значения.
  * @return Введенное значение типа int.
+ * Если ввод некорректен, программа выводит сообщение об ошибке и завершает выполнение.
  */
-long inputInt(const char* msg) {
+int inputInt(const char* msg) {
     int inputValue = 0;
     if (msg != NULL) {
         printf(msg);
     }
-    
     int result = scanf("%d", &inputValue);
     if (result != 1) {
         errno = EIO;
@@ -64,19 +66,11 @@ long inputInt(const char* msg) {
     return inputValue;
 }
 
-/**
- * @brief Функция для ввода значения типа double с сообщением.
- * Эта функция запрашивает у пользователя ввод числа с плавающей точкой, выводя заданное сообщение.
- * Если ввод некорректен, программа выводит сообщение об ошибке и завершает выполнение.
- * @param msg Сообщение, которое будет выведено перед вводом значения.
- * @return Введенное значение типа double.
- */
 double inputDouble(const char* msg) {
     double inputValue = 0;
     if (msg != NULL) {
         printf(msg);
     }
-    
     int result = scanf("%lf", &inputValue);
     if (result != 1) {
         errno = EIO;
@@ -84,4 +78,36 @@ double inputDouble(const char* msg) {
         exit(EXIT_FAILURE);
     }
     return inputValue;
+}
+
+/**
+ * @brief Вычисляет сумму членов последовательности.
+ * @param n Число элементов последовательности.
+ */
+double getSumOfSequence(int n) {
+    double res = 0.0;
+    double term = -1.0;
+    for (int k = 1; k <= n; k++) {
+        res += term;
+        term *= -1.0 / k;
+    }
+    return res;
+}
+
+/**
+ * @brief Вычисляет сумму членов последовательности,
+ *        которые выше по модулю чем epsilon.
+ * @param n Число элементов последовательности.
+ * @param eps Значение epsilon.
+ */
+double getSumOfSequenceMoreThanEps(int n, double eps) {
+    double resLessThanEps = 0.0;
+    double term = -1.0;
+    for (int k = 1; k <= n; k++) {
+        if (abs(term) >= eps) {
+            resLessThanEps += term;
+        }
+        term *= -1.0 / k;
+    }
+    return resLessThanEps;
 }
